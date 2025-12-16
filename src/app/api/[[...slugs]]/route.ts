71b1083d998faf971daac99a689790854c0c1e8c
Ryan Mello <ryanmello897@gmail.com>
2025-12-16 15:40:10 -0800

    create room

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,7 +1,20 @@
+import { redis } from "@/lib/redis";
 import { Elysia } from "elysia";
+import { nanoid } from "nanoid";
 
-const rooms = new Elysia({ prefix: "/room" }).post("/create", () => {
-  console.log("create a new room");
+const ROOM_TTL_IN_SECONDS = 60 * 10;
+
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_IN_SECONDS);
+
+  return { roomId };
 });
 
 const app = new Elysia({ prefix: "/api" }).use(rooms);
